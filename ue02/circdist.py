fb3186240267d5783a5b453f2c0a7f32cccd1d7d
Robin Nehls <nehls@mi.fu-berlin.de>
2013-05-15 22:32:47 +0200

    added task 2

--- ue02/circdist.py
+++ ue02/circdist.py
@@ -1,24 +1,53 @@
-#!/usr/bin/env python2
-import matplotlib
+#!/usr/bin/env python3
 import math
-import cmath
 import random
+import matplotlib.pyplot as plot
 
-def generate_point():
+
+def generate_point(r):
     phi = random.uniform(-math.pi, math.pi)
-    return phi
+    x = math.cos(phi) * r
+    y = math.sin(phi) * r
+    return (x, y)
+
 
 def distance(p1, p2):
-    d = abs(p1-p2)
-    if d > math.pi:
-        d = 2*math.pi-d
-    return math.sqrt(d)
+    x = abs(p1[0]-p1[1])
+    y = abs(p2[0]-p2[1])
+    return math.sqrt(x**2 + y**2)
 
-def simulate(point_num):
-    points = [generate_point() for _ in range(point_num)]
+
+def simulate(point_num, r, plotit=False):
+    points = [generate_point(r) for _ in range(point_num)]
+
+    if plotit:
+        xs = []
+        ys = []
+        for p in points:
+            xs.append(p[0])
+            ys.append(p[1])
+            plot.plot(xs, ys, 'r+')
+            plot.show()
+
+    point = generate_point(r)
     d = []
-    for p1 in points:
-        for p2 in points:
-            if p1 != p2:
-                d.append(distance(p1, p2))
+    for p in points:
+            d.append(distance(p, point))
     return d
+
+
+def avg(a):
+    return float(sum(a)) / len(a)
+
+
+r = 10
+num = 100
+while num <= 100000:
+    print('-'*40)
+    print('Simulating with %d points at r=%d' % (num, r))
+    d = simulate(num, r)
+    print('Result:    %f' % avg(d))
+    print('Should be: %f' % ((128/(45*math.pi))*r))
+    plot.hist(d,100)
+    plot.show()
+    num *= 10
